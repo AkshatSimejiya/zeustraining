1f98501ed901b7f9a6dcc6bd88a6ff0a6d3128f0
AkshatSimejiya <akshat.simejiya@zeuslearning.com>
2025-06-06 14:21:20 +0530

    Few Updates and Modifications Task -4 Done

--- task-3/script.js
+++ task-3/script.js
@@ -7,31 +7,43 @@ function factorial(N){
     for(let x = 2; x<=N;x++){
         size = multiply(fact, size, x)
     }
-
-    fact.reverse();
-    let ans = fact.join('');
+    
+    let ans = fact[fact.length-1].toString();
+    for(let i = fact.length - 2; i>=0; i--){
+        ans+=fact[i].toString().padStart(4,'0')
+    }
+    
+    console.log(fact.length)
+    console.log(ans.length)
     return ans;
 }
 
 function multiply(res, size, x){
     let carry = 0;
+    let base = 10000;
 
     for(let i = 0; i<size; i++){
         let product = res[i] * x + carry;
 
-        res[i] = product % 10;
+        res[i] = product % base;
 
-        carry = Math.floor(product / 10);
+        carry = Math.floor(product / base);
     }
 
-    while (carry)
+    while (carry) 
     {
-        res[size] = carry%10;
-        carry = Math.floor(carry/10);
+        res[size] = carry%base;
+        carry = Math.floor(carry/base);
         size++;
     }
 
     return size;
 }
 
-console.log(factorial(1000))
+console.time("Time");
+
+const ans = factorial(1000)
+
+console.timeEnd("Time");
+
+console.log(ans)
